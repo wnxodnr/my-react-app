49e825a0b1632132b8cd95f6b8c1c3c302c9e265
wnxodnr <wnxodnr@naver.com>
2023-01-03 10:30:55 +0900

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Let's Learn React
         </a>
       </header>
     </div>
